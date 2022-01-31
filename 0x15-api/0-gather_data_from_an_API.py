254993728fe78f15cb343599ea82606328d785ba
Andres Castañeda <andrescastanedaxy@gmail.com>
2022-01-31 14:03:05 -0500

    [ADD] - Creation code and file task 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Get info of https://jsonplaceholder.typicode.com"""
+
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    employee = ''
+    task_complete = 0
+    array_task_complete = []
+    tasks = 0
+    page = "https://jsonplaceholder.typicode.com/users/{}".format(sys.argv[1])
+
+    req = requests.get(page)
+    employee = req.json()['name']
+
+    page = ("https://jsonplaceholder.typicode.com/todos?userId={}".format
+            (sys.argv[1]))
+    req = requests.get(page)
+    for task in req.json():
+        tasks += 1
+        if task.get('completed') is True:
+            task_complete += 1
+            array_task_complete.append(task.get('title'))
+    print(
+        f'Employee Ervin Howell is done with tasks({task_complete}/{tasks}):')
+    for task in array_task_complete:
+        print(f'\t{task}')
