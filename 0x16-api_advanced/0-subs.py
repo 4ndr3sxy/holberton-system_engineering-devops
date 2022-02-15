eddbab9dbdcae8237d43e132c8cc104a4e8f7d98
Andres Castañeda <andrescastanedaxy@gmail.com>
2022-02-15 13:09:31 -0500

    [FIX] - logic code

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-"""Get subcribes in an specific sudreddit"""
+""" This module requests Reddit API for suscribers """
 import json
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Structure using module requests"""
-    user_agent = {'User-agent': 'Mozilla/5.0',
-                  "Content-Type": "application/json"}
-    page = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(page, headers=user_agent,
-                            allow_redirects=False)
-    if response.status_code == 200:
-        info = json.loads(response.content)
-        return info['data']['subscribers']
-    return 0
+    """ Return the number of subscribers. """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    header = {"Content-Type": "application/json",
+              "User-Agent": "Mozilla/5.0"}
+    request = requests.get(
+        url,
+        headers=header,
+        allow_redirects=False)
+    if request.status_code >= 300:
+        return 0
+    return json.loads(request.content.decode("utf-8"))["data"]["subscribers"]
