464c5d1233e5e3eb351e80b25a5b311bcb837e42
Andres Castañeda <andrescastanedaxy@gmail.com>
2022-02-15 14:45:55 -0500

    [UPD] - get() used

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
 """Get subcribes in an specific sudreddit"""
-import json
 import requests
 
 
 def number_of_subscribers(subreddit):
     """Structure using module requests"""
-    user_agent = {'User-agent': 'Mozilla/5.0',
-                  'Content-Type': 'application/json'}
+    user_agent = {'User-agent': 'Mozilla/5.0'}
     page = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(page, headers=user_agent,
                             allow_redirects=False)
     if response.status_code == 404:
         return 0
-    info = json.loads(response.content)
-    return info['data']['subscribers']
+    info = response.json()
+    return info.get('data').get('subscribers')
