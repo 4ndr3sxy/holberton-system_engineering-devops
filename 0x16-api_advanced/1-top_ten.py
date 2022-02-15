ca1831b19d00a6ea31c3273ae94242331343a30a
Andres Castañeda <andrescastanedaxy@gmail.com>
2022-02-15 14:51:48 -0500

    [UPD] - get() used

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
-
+"""Get top 10 articles in a specific sudreddit"""
 import json
 import requests
 
 
 def top_ten(subreddit):
+    """Get first 10 articles using requests"""
     user_agent = {'User-agent': 'Mozilla/5.0'}
     page = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    response = requests.get(page, headers=user_agent)
-    if response.status_code == 200:
+    response = requests.get(page, headers=user_agent,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print(None)
+    else:
         info = json.loads(response.content)
         count = 0
-        for post in info['data']['children']:
-            print(post['data']['title'])
+        for post in info.get('data').get('children'):
+            print(post.get('data').get('title'))
             count += 1
             if count >= 10:
                 break
-    else:
-        print(None)
