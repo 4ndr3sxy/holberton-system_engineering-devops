4db9f362e8ec023470aa9e2b1a47c5a96cf5ce5b
Andres Castañeda <andrescastanedaxy@gmail.com>
2022-02-15 12:52:39 -0500

    [UPD] - doocumentation

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-
+"""Get subcribes in an specific sudreddit"""
 import json
 import requests
 
 
 def number_of_subscribers(subreddit):
-
+    """Structure using module requests"""
     user_agent = {'User-agent': 'Mozilla/5.0'}
     page = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(page, headers=user_agent)
