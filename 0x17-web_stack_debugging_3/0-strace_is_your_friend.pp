01b444081c947e6113593925b3a59140e29897fb
Andres Castañeda <andrescastanedaxy@gmail.com>
2022-02-16 14:00:19 -0500

    [DEL] - delete info irrelevant

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -3,9 +3,3 @@ exec { 'curl-command':
     command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php && sudo service apache2 restart',
     path    => ['/bin', '/usr/bin', '/usr/sbin']
 }
-
-
-curl -X GET "https://api.datadoghq.com/api/v1/hosts" \
--H "Content-Type: application/json" \
--H "DD-API-KEY: efb17bf66c12bfb34f788d744f3d337c" \
--H "DD-APPLICATION-KEY: 6beb4c7cd796c269212620ea40fdb28ae52ce167"
